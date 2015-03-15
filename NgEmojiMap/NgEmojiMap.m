c2946e648c42966c9a0969e217fa126464a8a155
Meiwin Fu <meiwin@blockthirty.com>
2015-03-15 13:15:09 +0800

    Initial Commit

---
+++ NgEmojiMap/NgEmojiMap.m
@@ -0,0 +1,13 @@
+//
+//  NgEmojiMap.m
+//  NgEmojiMap
+//
+//  Created by Meiwin Fu on 15/3/15.
+//  Copyright (c) 2015 Meiwin. All rights reserved.
+//
+
+#import "NgEmojiMap.h"
+
+@implementation NgEmojiMap
+
+@end
