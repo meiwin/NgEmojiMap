09d80f93dd93ab518e85bcb3cfac2076b0a7c6b8
meiwin.fu <meiwin.fu@bubblemotion.com>
2015-03-15 13:15:09 +0800

    Initial Commit

---
+++ NgEmojiMap/NgEmojiMap.h
@@ -0,0 +1,13 @@
+//
+//  NgEmojiMap.h
+//  NgEmojiMap
+//
+//  Created by Meiwin Fu on 15/3/15.
+//  Copyright (c) 2015 Meiwin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NgEmojiMap : NSObject
+
+@end
